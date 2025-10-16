f468be3e5870c8e31f8a5f3b239f75bf6b3714d3
Marcus Mikael <marcusmikael2@gmail.com>
2025-10-16 20:54:11 -0300

    Implementação da lista duplamente encadeada

--- ListaDupla/src/listadupla/Main.java
+++ ListaDupla/src/listadupla/Main.java
@@ -4,17 +4,64 @@
  */
 package listadupla;
 
-/**
- *
- * @author anail
- */
+import java.util.Scanner;
+
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        ListaDuplamenteEncadeada lista = new ListaDuplamenteEncadeada();
+        int opcao;
+
+        do {
+            System.out.println("\n--- MENU LISTA DUPLAMENTE ENCADEADA ---");
+            System.out.println("1 - Inserir");
+            System.out.println("2 - Buscar (Consultar)");
+            System.out.println("3 - Listar do Início ao Fim");
+            System.out.println("4 - Listar do Fim ao Início");
+            System.out.println("5 - Remover");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha uma opção: ");
+            opcao = sc.nextInt();
+
+            switch (opcao) {
+                case 1 -> {
+                    System.out.print("Valor a inserir: ");
+                    int valor = sc.nextInt();
+                    lista.inserir(valor);
+                    System.out.println("Valor inserido: " + valor);
+                }
+                case 2 -> {
+                    System.out.print("Valor a buscar: ");
+                    int valor = sc.nextInt();
+                    boolean encontrado = lista.buscar(valor);
+                    if (encontrado) {
+                        System.out.println("Valor " + valor + " encontrado na lista.");
+                    } else {
+                        System.out.println("Valor " + valor + " não encontrado.");
+                    }
+                }
+                case 3 ->
+                    lista.listarInicio();
+                case 4 ->
+                    lista.listarFim();
+                case 5 -> {
+                    System.out.print("Valor a remover: ");
+                    int valor = sc.nextInt();
+                    boolean removido = lista.remover(valor); // ou excluir(), dependendo do seu nome
+                    if (removido) {
+                        System.out.println("Valor " + valor + " removido.");
+                    } else {
+                        System.out.println("Valor " + valor + " não encontrado.");
+                    }
+                }
+                case 0 ->
+                    System.out.println("Saindo...");
+                default ->
+                    System.out.println("Opção inválida. Tente novamente!");
+            }
+        } while (opcao != 0);
+
+        sc.close();
     }
-    
 }
